52ccd86a1b14e5c4a8bffc363613f207393992d8
Zubair Gaj <humagaj@humas-mbp.home>
2018-08-04 23:02:18 +0100

    Updated Look

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
