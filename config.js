58563ce9b01bbd432dda5c5dde09e453243ce673
ZubairG <21358932+ZubairGaj@users.noreply.github.com>
2018-08-14 16:05:57 +0100

    fixing

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const passport = require('passport');
 const ebayauth = require('passport-ebay');
 
 passport.use(new eBayStrategy({
-    clientID: EBAY_APP_ID,
-    clientSecret: EBAY_APP_SECRET,
-    ruName: EBAY_APP_REDIRECT_URL_NAME
+    clientID: 'ZubairGa-hoodedso-SBX-c5d8a3c47-8b93d42a',
+    clientSecret: 'SBX-5d8a3c47277e-866c-430e-9847-077d',
+    ruName: '/ebay/redirect'
   },
   function(accessToken, refreshToken, cb) {
     // Do whatever you need with credentials. A request call to eBay api to fetch user perhaps?
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WonderStone Group' });
 });
 
-router.get('/ebay/login', function(req, res, next) {
+router.get('/ebay', function(req, res, next) {
   res.render('login', { title: 'WonderStone Group' });
+});
+
+router.get('/ebay/logout', function(req, res, next) {
+  //handlewith passport
+  res.send('logout your ebay account');
+});
+
+router.get('/ebay/login', function(req, res, next) {
+  //handlewith passport
+  res.send('login with ebay');
+});
+
+router.get('/ebay/redirect', function(req, res, next) {
+  //handlewith passport
+  res.send('login with ebay');
 });
 
 module.exports = router;
