edc50faf39655e80e55de8767350163501da791c
ZubairG <21358932+ZubairGaj@users.noreply.github.com>
2018-08-16 11:43:06 +0100

    testing

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
-const eBayStrategy = require('passport-ebay').default;
+// const eBayStrategy = require('passport-ebay').default;
+const OAuth2Strategy = require('general-oauth2');
 const passport = require('passport');
 const keys = require('./keys');
 
-passport.use(new eBayStrategy({
+passport.use(new OAuth2Strategy({
+    authorizationURL: 'https://auth.ebay.com/oauth2/authorize',
+    tokenURL: 'https://api.ebay.com/identity/v1/oauth2/token',
     clientID: keys.ebay.clientID,
     clientSecret: keys.ebay.clientSecret,
     ruName: '/ebay/redirect'
--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,10 @@ router.get('/ebay', function(req, res, next) {
 });
 
 router.get('/ebay/auth',
-  passport.authenticate('ebay'));
+  passport.authenticate('oauth2'));
 
 router.get('/ebay/redirect',
-  passport.authenticate('ebay', { failureRedirect: '/login' }), function(req, res, next) {
+  passport.authenticate('oauth2', { failureRedirect: '/login' }), function(req, res, next) {
     res.redirect('/');
 });
 
