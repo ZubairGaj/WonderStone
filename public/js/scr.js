688481d464da3647b5eabb87a7c2c2450c66ad37
ZubairGaj <zubair.gaj@sky.uk>
2018-08-18 22:52:50 +0100

    Improved Js vallidation

--- public/js/scr.js
+++ public/js/scr.js
@@ -1,19 +1,27 @@
 function formSubmit(){
-
+  $( "#submit-message" ).empty();
   var textareaField = document.getElementById('inputTextarea');
   var emailField = document.getElementById('email');
   var nameField = document.getElementById('inputFullName');
 
   var body = "Name: " +  nameField.value + "   " + "Query: " + textareaField.value + "   " + "email: " + emailField.value;
 
-  var emailTo = emailField.value
-  var posting = $.post( '/query', { query: body }, ()=>{
-    console.log('lol');
-  });
-  posting.done(function( {data: body, email: emailTo} ) {
-    console.log('lolllll');
+
+  function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  var emailvalidation = validateEmail(emailField.value);
+
+  if (emailvalidation != true) {
+    $( "#submit-message" ).empty().append('Please enter a valid email address!')
+  } else if (textareaField.value < 1) {
+    $( "#submit-message" ).empty().append('Please enter a valid query.')
+  } else {
+    $( "#submit-message" ).empty();
+    var posting = $.post('/query', { query: body });
+    posting.done()
     $( "#submit-message" ).empty().append('Query successfully recieved!')
-    // document.getElementById('inputTextarea')
-    //
-  });
+  }
 }
