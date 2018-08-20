fed3bd55ce2b1bce5bec95cd94258cd515437fb5
ZubairGaj <zubair.gaj@sky.uk>
2018-08-20 14:30:28 +0100

    testing cors setup

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const logger = require('morgan');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
+var cors = require('cors')
 
 const app = express();
 
 mongoose.connect('mongodb://zubzg:zubair1998@ds225382.mlab.com:25382/ebaymanager', { useNewUrlParser: true });
 
 // view engine setup
+app.use(cors())
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'hbs');
